bb29bc052beb7700cdd1be5e8381836119776079
Steve Wang <stevewang@utexas.edu>
2018-02-15 13:20:58 -0600

    fixed total commit count

--- run.py
+++ run.py
@@ -61,8 +61,7 @@ def get_single_media(media_name: str):
 
 @app.route('/about')
 def get_about():
-    return render_template('about.html', commits=get_commits()[0], total_commits=get_commits()[1], 
-                             issues=get_issues()[0], total_issues=get_issues()[1])
+    return render_template('about.html', commits=get_commits()[0], total_commits=get_commits()[1], issues=get_issues()[0], total_issues=get_issues()[1])
 
 def get_commits():
     url = 'https://api.github.com/repos/connormlewis/idb/stats/contributors'
@@ -75,7 +74,7 @@ def get_commits():
         user_name = entry['author']['login']
         team[user_name] = total
         all_commits += total
-    return (team, total)
+    return (team, all_commits)
 
 def get_issues():
     url = 'https://api.github.com/repos/connormlewis/idb/issues?state=all&filter=all'
@@ -86,12 +85,10 @@ def get_issues():
     all_issues = 0
     team = {'stevex196x':0, 'TheSchaft':0, 'melxtru':0, 'aylish19':0, 'connormlewis':0, 'tsukkisuki':0}
     for entry in json_list:
-        #if 'pull_request' not in entry:
-        team[entry['user']['login']] += 1
-        all_issues += 1
+        if 'pull_request' not in entry:
+            team[entry['user']['login']] += 1
+            all_issues += 1
     return (team, all_issues)
-
-
 
 @app.errorhandler(404)
 def page_not_found(e):
