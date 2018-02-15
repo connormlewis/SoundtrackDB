5891ce267a108c613f66aad3dc39bf7e1e120d27
Steve Wang <stevewang@utexas.edu>
2018-02-14 23:27:58 -0600

    added GitHub scraping

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, render_template, abort
+import requests
 
 app = Flask('SoundtrackDB')
 
@@ -60,7 +61,37 @@ def get_single_media(media_name: str):
 
 @app.route('/about')
 def get_about():
-    return render_template('about.html')
+    return render_template('about.html', commits=get_commits()[0], total_commits=get_commits()[1], 
+                             issues=get_issues()[0], total_issues=get_issues()[1])
+
+def get_commits():
+    url = 'https://api.github.com/repos/connormlewis/idb/stats/contributors'
+    data = requests.get(url)
+    json_list = data.json()
+    all_commits = 0
+    team = {'stevex196x':0, 'TheSchaft':0, 'melxtru':0, 'aylish19':0, 'connormlewis':0, 'tsukkisuki':0}
+    for entry in json_list:
+        total = entry['total']
+        user_name = entry['author']['login']
+        team[user_name] = total
+        all_commits += total
+    return (team, total)
+
+def get_issues():
+    url = 'https://api.github.com/repos/connormlewis/idb/issues?state=all&filter=all'
+    #login_url = 'https://api.github.com/user?access_token=43c3a1848527941be68055b0744de7b0ac2ab2ca'
+    #data = requests.get(login_url)
+    data = requests.get(url)
+    json_list = data.json()
+    all_issues = 0
+    team = {'stevex196x':0, 'TheSchaft':0, 'melxtru':0, 'aylish19':0, 'connormlewis':0, 'tsukkisuki':0}
+    for entry in json_list:
+        #if 'pull_request' not in entry:
+        team[entry['user']['login']] += 1
+        all_issues += 1
+    return (team, all_issues)
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
