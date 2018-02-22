38c4dff04ce542b96d85d5c715d407678ac74bd5
Aylish Wrench <aylish.wrench@utexas.edu>
2018-02-21 22:34:17 -0600

    created stub states

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {UIRouter, UIView} from 'ui-router-react';
+import {router} from './router.config'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <UIRouter router={router}>
+    <div>
+      <App />
+      <UIView/>
+    </div>
+  </UIRouter>
+  , document.getElementById('root')
+);
 registerServiceWorker();
