384975f5e492c299e72305a84385ff415dc068db
Aylish Wrench <aylish.wrench@utexas.edu>
2018-02-24 14:59:17 -0600

    set up mocha for frontend

---
+++ frontend/scripts/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import { JSDOM } from 'jsdom';
+
+global.document = new JSDOM('<html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = {userAgent: 'node.js'};
+
+function noop() {
+	return {}; 
+}
+
+// prevent mocha tests from breaking when trying to require a css file
+require.extensions['.css'] = noop;
+require.extensions['.svg'] = noop; 
