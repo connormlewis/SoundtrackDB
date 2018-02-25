8d315edec80f9b7aa950478c06a0291bfbba0f75
Aylish Wrench <aylish.wrench@utexas.edu>
2018-02-24 18:44:14 -0600

    fixed typo

--- frontend/tests.js
+++ frontend/tests.js
@@ -14,7 +14,7 @@ describe('<App/>', function () {
 });
 
 // Navigation
-describe('<Navigation', function () {
+describe('<Navigation/>', function () {
   it('should render without crashing', function() {
     shallow(<Navigation/>); 
   });
