91d65a87891d74094afa2803bddef6cec335bcd0
Connor Lewis <cmlewis95@gmail.com>
2018-02-26 12:45:53 -0600

    Change nav links to UISref components

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
-import {
-  Navbar, 
-  NavbarBrand, 
-  Container, 
-  Nav, 
-  NavItem,
-  NavLink } from 'reactstrap'; 
+import { Navbar, NavbarBrand, Container, Nav, NavItem, NavLink } from 'reactstrap';
+import { UISref } from '@uirouter/react';
 
 class Navigation extends Component {
   render() {
     return (
-      <Navbar light className="navbar-expand-md" style={{backgroundColor: "#d0e2f2"}}>
+      <Navbar light className="navbar-expand-md" style={{ backgroundColor: "#d0e2f2" }}>
         <Container fluid>
-          <NavbarBrand href="/">SoundtrackDB</NavbarBrand>
-            <Nav className="navbar-collapse" navbar>
-              <NavItem>
-                <NavLink href="/album">Albums</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/artist">Artists</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/tv-movie">TV/Movies</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/about">About</NavLink>
-              </NavItem>
-            </Nav>
+          <UISref to="home">
+            <div className="navbar-brand" style={{ cursor: 'pointer' }}>SoundtrackDB</div>
+          </UISref>
+          <Nav className="navbar-collapse" navbar>
+            <NavItem>
+              <UISref to="albumHome">
+                <div className="nav-link" style={{ cursor: 'pointer' }}>Albums</div>
+              </UISref>
+            </NavItem>
+            <NavItem>
+              <UISref to="artistHome">
+                <div className="nav-link" style={{ cursor: 'pointer' }}>Artists</div>
+              </UISref>
+            </NavItem>
+            <NavItem>
+              <UISref to="mediaHome">
+                <div className="nav-link" style={{ cursor: 'pointer' }}>TV/Movies</div>
+              </UISref>
+            </NavItem>
+            <NavItem>
+              <UISref to="about">
+                <div className="nav-link" style={{ cursor: 'pointer' }}>About</div>
+              </UISref>
+            </NavItem>
+          </Nav>
         </Container>
       </Navbar>
-    ); 
+    );
   }
 }
 
