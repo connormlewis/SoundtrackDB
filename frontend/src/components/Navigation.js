1d5dfee97b47a08857512e3eb3a78b605b5967e4
Connor Lewis <cmlewis95@gmail.com>
2018-03-02 14:05:48 -0600

    Use UISref’s instead of links

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,39 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Navbar, Container, Nav, NavItem } from 'reactstrap';
-import { UISref } from '@uirouter/react';
+import { UISref, UISrefActive } from '@uirouter/react';
 
 class Navigation extends Component {
   render() {
     return (
-      <Navbar light className="navbar-expand-md" style={{ backgroundColor: "#d0e2f2" }}>
-        <Container fluid>
-          <UISref to="home">
-            <div className="navbar-brand" style={{ cursor: 'pointer' }}>SoundtrackDB</div>
-          </UISref>
-          <Nav className="navbar-collapse" navbar>
-            <NavItem>
-              <UISref to="albumHome">
-                <div className="nav-link" style={{ cursor: 'pointer' }}>Albums</div>
-              </UISref>
-            </NavItem>
-            <NavItem>
-              <UISref to="artistHome">
-                <div className="nav-link" style={{ cursor: 'pointer' }}>Artists</div>
-              </UISref>
-            </NavItem>
-            <NavItem>
-              <UISref to="mediaHome">
-                <div className="nav-link" style={{ cursor: 'pointer' }}>TV/Movies</div>
-              </UISref>
-            </NavItem>
-            <NavItem>
-              <UISref to="about">
-                <div className="nav-link" style={{ cursor: 'pointer' }}>About</div>
-              </UISref>
-            </NavItem>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Fragment>
+        <Navbar light className="navbar-expand-md" style={{ backgroundColor: "#d0e2f2" }}>
+          <Container fluid>
+            <UISref to="home">
+              <div className="navbar-brand" style={{ cursor: 'pointer' }}>SoundtrackDB</div>
+            </UISref>
+            <Nav className="navbar-collapse" navbar>
+              <UISrefActive class="active">
+                <NavItem>
+                  <UISref to="albumHome">
+                    <div className="nav-link" style={{ cursor: 'pointer' }}>Albums</div>
+                  </UISref>
+                </NavItem>
+              </UISrefActive>
+              <UISrefActive class="active">
+                <NavItem>
+                  <UISref to="artistHome">
+                    <div className="nav-link" style={{ cursor: 'pointer' }}>Artists</div>
+                  </UISref>
+                </NavItem>
+              </UISrefActive>
+              <UISrefActive class="active">
+                <NavItem>
+                  <UISref to="mediaHome">
+                    <div className="nav-link" style={{ cursor: 'pointer' }}>TV/Movies</div>
+                  </UISref>
+                </NavItem>
+              </UISrefActive>
+              <UISrefActive class="active">
+                <NavItem>
+                  <UISref to="about">
+                    <div className="nav-link" style={{ cursor: 'pointer' }}>About</div>
+                  </UISref>
+                </NavItem>
+              </UISrefActive>
+            </Nav>
+          </Container>
+        </Navbar>
+      </Fragment>
     );
   }
 }
