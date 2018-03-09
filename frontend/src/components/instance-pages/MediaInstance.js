af820eeb76a2d33ca05cd437c25f2f8c7913f20a
Aylish Wrench <aylish.wrench@utexas.edu>
2018-03-08 18:00:23 -0600

    finished media instance pages

--- frontend/src/components/instance-pages/MediaInstance.js
+++ frontend/src/components/instance-pages/MediaInstance.js
@@ -1,27 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import { Row, Col } from 'reactstrap'; 
 import PropTypes from 'prop-types'
+import { UISref } from '@uirouter/react';
+import { MediaCarousel } from './MediaCarousel'; 
 
 export class MediaInstance extends Component {
-  getTitle() {
-    let model_data = this.props.media.model_data; 
-
-    if(model_data.name) {
-      return model_data.name; 
-    }
-    else {
-      return model_data.title; 
-    }
-  }
 
   getSubtitle() {
-    let model_data = this.props.media.model_data; 
-    if(model_data.name) {
-      let first = model_data.first_air_date.substring(0, 4);  
-      let last = model_data.last_air_date.substring(0, 4); 
-      if (last === '2018') {
+    let model_data = this.props.media;
+    if(model_data.type === 'show') {
+      let first = model_data.years[0];  
+      let last = ''; 
+      if (model_data.running === true) {
         last = 'Present'; 
       }
+      else {
+        last = model_data.years[model_data.years.length - 1]; 
+      }
+
       let years = ''; 
       if (first === last) {
         years = first; 
@@ -30,20 +26,42 @@ export class MediaInstance extends Component {
         years = first + ' - ' + last; 
       }
 
-      let seasons = model_data.seasons.length; 
-      let genres = model_data.genres.map(function(genre){return genre.name;}).join(', ');
+      let seasons = model_data.seasons; 
+      let genres = model_data.genres.join(', ');
       return 'TV Show | ' + years + ' | ' + seasons + " seasons | " + genres; 
     }
     else {
-      let year = model_data.release_date.substring(0, 4); 
-      let genres = model_data.genres.map(function(genre){return genre.name;}).join(', ');
+      let year = model_data.years; 
+      let genres = model_data.genres.join(', ');
       return 'Movie | ' + year + ' | ' + genres; 
     }
   }
 
   getCast() {
-    const cast = this.props.media.cast_data.cast; 
-    return cast.map((member, index) => <li key={index}>{member.name}</li>); 
+    const cast = this.props.media.cast; 
+    return cast.map((member, index) => <li key={index}>{member}</li>); 
+  }
+
+  getAlbums() {
+    const albums = this.props.media.albums; 
+    return (
+      <li key={albums[0]}>
+        <UISref to="albumInstance" params={{albumID:albums[0]}}>
+          <a>{albums[1]}</a>
+        </UISref>
+      </li>
+    ); 
+  }
+
+  getArtists() {
+    const artists = this.props.media.artists; 
+    return (
+      <li key={artists[0]}>
+        <UISref to="artistInstance" params={{artistID:artists[0]}}>
+          <a>{artists[1]}</a>
+        </UISref>
+      </li>
+    ); 
   }
 
   render () {
@@ -51,32 +69,34 @@ export class MediaInstance extends Component {
       <Fragment>
         <Row className="pb-2">
           <Col sm="8">
-            <h1 id="name">{this.getTitle()}</h1>
+            <h1 id="name">{this.props.media.name}</h1>
             <h4 id="subtitle">{this.getSubtitle()}</h4>
             <h3>About</h3>
-            <p id="about">{this.props.media.model_data.overview}</p>
+            <p id="about">{this.props.media.overview}</p>
             <Row className="pb-2">
               <Col sm="6">
                 <h3>Cast</h3>
                 <ul id="cast">{this.getCast()}</ul>
+                <h3>Photos</h3>
+                <MediaCarousel photos={this.props.media.backdrops}/>
               </Col>
             </Row>
           </Col> 
           <Col sm="4">
-            <img className="w-100" src={"http://image.tmdb.org/t/p/w500/" + this.props.media.model_data.poster_path} alt="Poster" vspace="20"/>
+            <img className="w-100" src={this.props.media.poster} alt="Poster" vspace="20"/>
             <div className="embed-responsive embed-responsive-16by9 w-100">
-              <iframe className="embed-responsive-item" title="Trailer" src={"//www.youtube.com/embed/" + this.props.media.video_data.results[0].key} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen />
+              <iframe className="embed-responsive-item" title="Trailer" src={"//www.youtube.com/embed/" + this.props.media.video.key} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen />
             </div>
           </Col>
         </Row>
         <Row>
           <Col sm="6">
             <h3>Soundtracks</h3>
-            <ul id="albums"></ul>
+            <ul id="albums">{this.getAlbums()}</ul>
           </Col>
           <Col sm="6">
             <h3>Artists</h3>
-            <ul id="artists"></ul>
+            <ul id="artists">{this.getArtists()}</ul>
           </Col>
         </Row>
       </Fragment>
