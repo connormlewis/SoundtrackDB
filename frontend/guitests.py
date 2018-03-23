62e06b26606eab7a8869b7c1ad2e8b1050c31b35
Steve Wang <stevewang@utexas.edu>
2018-03-22 20:20:12 -0500

    uncommented out the tests lol

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -18,7 +18,6 @@ def test_navbar(tester):
     driver = tester.driver
     driver.get(tester.url)
     html = list(driver.find_elements_by_tag_name("body"))[0].get_attribute('outerHTML')
-    
     tabs = list(driver.find_elements_by_tag_name("li"))
     tester.assertIn("Albums", tabs[0].get_attribute("outerHTML"))
     tester.assertIn("Artists", tabs[1].get_attribute("outerHTML"))
@@ -37,73 +36,72 @@ def test_card_items(tester):
     driver = tester.driver
     driver.get(tester.url)
     html = list(driver.find_elements_by_tag_name("body"))[0].get_attribute('outerHTML')
-    
     tabs = list(driver.find_elements_by_tag_name("img"))
 
-# class HomePage(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = setup_driver()
-#         self.url = "http://localhost:3000"
+class HomePage(unittest.TestCase):
+    def setUp(self):
+        self.driver = setup_driver()
+        self.url = "http://localhost:3000"
 
-#     def test_welcome_message(self):
-#         test_title(self, "Welcome to SoundtrackDB");
+    def test_welcome_message(self):
+        test_title(self, "Welcome to SoundtrackDB");
 
-#     def test_navigation(self):
-#         test_navbar(self);
+    def test_navigation(self):
+        test_navbar(self);
 
-#     def tearDown(self):
-#         self.driver.close()
+    def tearDown(self):
+        self.driver.close()
 
-# class AlbumPage(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = setup_driver()
-#         self.url = "http://localhost:3000/album"
+class AlbumPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = setup_driver()
+        self.url = "http://localhost:3000/album"
 
-#     def test_title(self):
-#         test_title(self, "Albums");
+    def test_title(self):
+        test_title(self, "Albums");
 
-#     def test_navigation(self):
-#         test_navbar(self);
+    def test_navigation(self):
+        test_navbar(self);
 
-#     def test_cards(self):
-#         test_card_items(self)
+    def test_cards(self):
+        test_card_items(self)
 
-#     def tearDown(self):
-#         self.driver.close()
+    def tearDown(self):
+        self.driver.close()
 
-# class ArtistPage(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = setup_driver()
-#         self.url = "http://localhost:3000/artist"
+class ArtistPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = setup_driver()
+        self.url = "http://localhost:3000/artist"
 
-#     def test_title(self):
-#         test_title(self, "Artists");
+    def test_title(self):
+        test_title(self, "Artists");
 
-#     def test_navigation(self):
-#         test_navbar(self);
+    def test_navigation(self):
+        test_navbar(self);
 
-#     def test_cards(self):
-#         test_card_items(self)
+    def test_cards(self):
+        test_card_items(self)
 
-#     def tearDown(self):
-#         self.driver.close()
+    def tearDown(self):
+        self.driver.close()
 
-# class MediaPage(unittest.TestCase):
-#     def setUp(self):
-#         self.driver = setup_driver()
-#         self.url = "http://localhost:3000/media"
+class MediaPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = setup_driver()
+        self.url = "http://localhost:3000/media"
 
-#     def test_title(self):
-#         test_title(self, "Media - Movies/TV Series");
+    def test_title(self):
+        test_title(self, "Media - Movies/TV Series");
 
-#     def test_navigation(self):
-#         test_navbar(self);
+    def test_navigation(self):
+        test_navbar(self);
 
-#     def test_cards(self):
-#         test_card_items(self)
+    def test_cards(self):
+        test_card_items(self)
 
-#     def tearDown(self):
-#         self.driver.close()
+    def tearDown(self):
+        self.driver.close()
 
 class AboutPage(unittest.TestCase):
     def setUp(self):
