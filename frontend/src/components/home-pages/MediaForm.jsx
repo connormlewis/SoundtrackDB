74030a2075af90a05b59ffe66d17e9125d25b3f2
Aylish Wrench <aylish.wrench@utexas.edu>
2018-04-05 20:01:06 -0500

    fixed checkboxes

--- frontend/src/components/home-pages/MediaForm.jsx
+++ frontend/src/components/home-pages/MediaForm.jsx
@@ -7,8 +7,9 @@ export class MediaForm extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {start_year: 0, end_year: 2018}; 
+    this.state = {movie: false, tv_show: false, start_year: 0, end_year: 2018, running: false}; 
     this.filter = this.filter.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   filter(params) {
@@ -28,6 +29,16 @@ export class MediaForm extends Component {
     stateService.go('^.home', {limit: 12, offset: 0, filters: filters});
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render () {
     return (
       <Form className="filtering-form">
@@ -35,13 +46,13 @@ export class MediaForm extends Component {
         <FormGroup check>
           <Col sm={{ size: 20}}>
             <Label check>
-              <Input type="checkbox" defaultChecked={this.state.movie} onChange={(e) => (this.setState({movie: !e.target.value}))}/>{' '}
+              <input name="movie" type="checkbox" checked={this.state.movie} onChange={this.handleInputChange}/>{' '}
               Movie
             </Label>
           </Col>
           <Col sm={{ size: 20}}>
             <Label check>
-              <Input type="checkbox" onChange={(e) => (this.setState({tv_show: !e.target.value}))}/>{' '}
+              <input name="tv_show" type="checkbox" checked={this.state.tv_show} onChange={this.handleInputChange}/>{' '}
               TV Show
             </Label>
           </Col>
@@ -60,7 +71,7 @@ export class MediaForm extends Component {
         <FormGroup check>
           <Col sm={{ size: 20}}>
             <Label check>
-              <Input type="checkbox" onChange={(e) => (this.setState({running: !e.target.value}))}/>{' '}
+              <input name="running" type="checkbox" checked={this.state.running} onChange={this.handleInputChange}/>{' '}
               Currently Running Series
             </Label>
           </Col>
