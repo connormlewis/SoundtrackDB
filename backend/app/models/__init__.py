c1338b751a898596f7c329436b5ee98769e84cbb
Connor Lewis <cmlewis95@gmail.com>
2018-04-16 16:02:45 -0500

    Split genre into it’s own table
    Relates to #181

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.album import Album, AlbumSchema
 from app.models.artist import Artist, ArtistSchema
 from app.models.media import Media, MediaSchema
 from app.models.search import search, SearchSchema
+from app.models.genre import Genre, GenreSchema
