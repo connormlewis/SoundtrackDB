ee37da52658fb03492753b899a49ef8d93303500
Alan Yi <tsukkisuki@utexas.edu>
2018-04-15 23:33:31 -0500

    fixed navbar in mobile ... for the most part

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,11 +1,26 @@
 import React, { Component, Fragment } from 'react';
-import { Navbar, Container, Nav, NavItem } from 'reactstrap';
+import { Navbar, Container, Nav, NavItem, NavbarToggler, Collapse } from 'reactstrap';
 import { UISref, UISrefActive } from '@uirouter/react';
 import SearchBar from './SearchBar';
 import "../style/Navigation.css";
 import img from '../static/logo.png'
 
 class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
 
   search(searchTerm) {
     const { stateService } = this.props.router;
@@ -22,39 +37,44 @@ class Navigation extends Component {
                 <img className="nav-logo" src={img} alt="logo"></img>
               </div>
             </UISref>
-            <Nav className="navbar-collapse" navbar>
-              <UISrefActive class="active">
-                <NavItem>
-                  <UISref to="album">
-                    <div className="nav-link" style={{ cursor: 'pointer' }}>Albums</div>
-                  </UISref>
-                </NavItem>
-              </UISrefActive>
-              <UISrefActive class="active">
-                <NavItem>
-                  <UISref to="artist">
-                    <div className="nav-link" style={{ cursor: 'pointer' }}>Artists</div>
-                  </UISref>
-                </NavItem>
-              </UISrefActive>
-              <UISrefActive class="active">
-                <NavItem>
-                  <UISref to="media">
-                    <div className="nav-link" style={{ cursor: 'pointer' }}>Media</div>
-                  </UISref>
-                </NavItem>
-              </UISrefActive>
-              <UISrefActive class="active">
-                <NavItem>
-                  <UISref to="about">
-                    <div className="nav-link" style={{ cursor: 'pointer' }}>About</div>
-                  </UISref>
-                </NavItem>
-              </UISrefActive>
-            </Nav>
-            <Nav className="ml-auto">
-              <SearchBar onSubmit={(searchTerm) => this.search(searchTerm)} placeholder="Search Everywhere"/>
-            </Nav>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse isOpen={!this.state.collapsed} navbar>
+              <Nav className="navbar-collapse" navbar>
+                <UISrefActive class="active">
+                  <NavItem>
+                    <UISref to="album">
+                      <div className="nav-link" style={{ cursor: 'pointer' }}>Albums</div>
+                    </UISref>
+                  </NavItem>
+                </UISrefActive>
+                <UISrefActive class="active">
+                  <NavItem>
+                    <UISref to="artist">
+                      <div className="nav-link" style={{ cursor: 'pointer' }}>Artists</div>
+                    </UISref>
+                  </NavItem>
+                </UISrefActive>
+                <UISrefActive class="active">
+                  <NavItem>
+                    <UISref to="media">
+                      <div className="nav-link" style={{ cursor: 'pointer' }}>Media</div>
+                    </UISref>
+                  </NavItem>
+                </UISrefActive>
+                <UISrefActive class="active">
+                  <NavItem>
+                    <UISref to="about">
+                      <div className="nav-link" style={{ cursor: 'pointer' }}>About</div>
+                    </UISref>
+                  </NavItem>
+                </UISrefActive>
+              </Nav>
+              <Nav>
+                <div className="search-bar">
+                <SearchBar onSubmit={(searchTerm) => this.search(searchTerm)} placeholder="Search Everywhere"/>
+                </div>
+              </Nav>
+            </Collapse>
           </Container>
         </Navbar>
       </Fragment>
